a1e586003ecdc0da9ac9c837d9d8927b4650369e
Okechukwu Peter .C <okechukwupeter10@gmail.com>
2025-09-20 13:30:07 +0100

    big

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -194,7 +194,7 @@ const ProductDetail = () => {
                 disabled={product.quantity === 0}
               >
                 <ShoppingCart className="mr-2 h-5 w-5" />
-                Add to C
+                Add to Cart
               </Button>
             </div>
           </div>
